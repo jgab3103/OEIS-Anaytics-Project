24581b2541e2fea059552602e9a09f4e2db23a4e
jamie.gabriel <jamie.gabriel@health.nsw.gov.au>
2021-09-26 19:06:26 +1000

    Update Main.R

--- DataPrep/Main.R
+++ DataPrep/Main.R
@@ -1,74 +1,66 @@
 library(tidyverse)
 library(lubridate)
 library(httr)
-library(XML)
 library(dplyr)
 library(xml2)
 
 
-###OVERVIEW
 
 #build the url and GET the result
 url <- "https://oeis.org/A005408"
 h <- handle(url)
 res <- GET(handle = h)
-
 a<- read_html(res)
-
 htmlCode <- xml_find_all(a, ".//body")
 
 
-
-
-
-
-sectGive <- function(node) {
-  # MAYBE AS FUCNTION
-}
-
-
-
+# create empty frame
 df <- data.frame(header = character(0),
                  pText = character(0))
 
+
+# get all td tags
 td <- xml_find_all(htmlCode, ".//td")
 
 
+
+# Iterate through all td tags
 for (x in td) {
   
-
   if (length(xml_find_first(x, ".//font")) > 0) {
+    
     section <- xml_text(xml_find_first(x, ".//font"))
     print(section)
-    
-  } else {
-    section <- "Not defined"
   }
   
+
+  # if there are p tags in this cell
   if(length(xml_find_all(x, ".//p")) > 0) {
+    
+    
     pTags <- xml_find_all(x, ".//p")
 
     
-    print(section)
-    
     for (p in pTags) {
+      print(paste0("Section to add in DataFrame -->", section))
+      print(paste0("PTag to add -->", as.character(p)))
       
       dataRow <- data.frame(
         header = section,
-         pText = xml_text(p)
+         pText = as.character(p)
          
          )
         df <- rbind(df, dataRow)
       
     }
     
-    print("END")
+    print("END OF ITERATING THROUGH PTAGS")
+
     #print(xml_text(pTags))
   }
   
-  # if (xml_length(x) > 5) {
-  #   print(xml_text(x, trim = TRUE))
-  # }
+
 }
 
 glimpse(df)
+
